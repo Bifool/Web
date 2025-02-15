6e665a69c9c9cfee968f12c7566a4e7109df72c9
Bifool <ladancsik54@gmail.com>
2025-02-15 16:23:01 +0000

    Feb 15, 2025, 5:23 PM

---
+++ app.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="p-4">
+            <h1 id="index" className="text-4xl font-bold">Hail</h1>
+            <h2 id="index" className="text-3xl font-semibold">Hail</h2>
+            <h3 id="index" className="text-2xl font-medium">Hail</h3>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
